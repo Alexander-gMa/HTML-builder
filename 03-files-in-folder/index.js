ee702cf56f3331801d734e47b511aa4f8cf5e83b
Alexander-gMa <svincickiaa@gmail.com>
2021-11-03 22:40:18 +0300

    feat: solve 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,28 +3,48 @@
 const fs = require('fs');
 const path = require('path');
 const testFolder = path.join(__dirname, "secret-folder");
-let nameFile;
-let extFile;
-let abc ;
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    nameFile = file;
-    extFile = path.extname(file);
-    nameOhneExt = nameFile.length - extFile.length;
-    console.log(nameFile.substr(0,nameOhneExt) + " - " + extFile.substr(1,extFile.length) + abc);
+
+fs.readdir(testFolder,{withFileTypes:true}, (err, files) => {
+  if (err) throw err;
+  files.forEach((file) => {
+    if(file.isFile()){
+      const nameFile = file.name;
+      const extFile = path.extname(file.name);
+     
+      const stat = fs.stat(path.join(testFolder, file.name),(err, el) => {
+        if (err) throw err;
+        const fileSize = el.size / 1024 ;
+        console.log(nameFile.substr(0,nameFile.length - extFile.length) + " - " + extFile.substr(1,extFile.length) + " - " + fileSize + "kb");
+      })
+    }
   });
 })
 
 
+// const {
+//   promises: { stat, readdir },
+// } = require("fs");
+// const path = require("path");
 
-var stats = fs.stat("03-files-in-folder/secret-folder/data.csv", (err, files) => {
-    if (err) {
-        console.log(err);
-        return;
-      }
-    abc = files.size;
-      // else show size from stats object
-      console.log("File Size is: ", abc / 1024 , "kb");
-      return abc
-});
+// const testFolder = path.join(__dirname, "secret-folder");
 
+// async function doSomething() {
+//   const dirEntries = await readdir(testFolder, { withFileTypes: true });
+//   const files = dirEntries.filter((entry) => entry.isFile());
+
+//   files.forEach(async (file) => {
+//     const nameFile = file.name;
+//     const extFile = path.extname(nameFile);
+
+//     const el = await stat(path.join(testFolder, nameFile));
+//     console.log(
+//       [
+//         nameFile.substr(0, nameFile.length - extFile.length),
+//         extFile.substr(1),
+//         `${el.size / 1024}kb`,
+//       ].join(" - ")
+//     );
+//   });
+// }
+
+// doSomething();
