9571fcb0c181a1b01b16e3434cb368490db4640b
Alexander-gMa <svincickiaa@gmail.com>
2021-11-06 13:34:45 +0300

    feat: corrected 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,17 +4,22 @@ const path = require('path');
 const src = path.join(__dirname, "files");
 const dest = path.join(__dirname, "files-copy");
 
-fs.mkdir(dest, { recursive: true }, (err, files) => {
-    if (err) throw err;
-    fs.readdir(src, { withFileTypes: true }, (err, files) => {
-        if (err) throw err;
-        files.forEach((file) => {
-            if (file.isFile()) {
-                fs.copyFile(path.join(src,file.name), path.join(dest,file.name), (err,files) => {
-                    if (err) throw err;
+fs.rm(dest, { recursive: true }, err => {
+    if (!err || err) {
+        fs.mkdir(dest, { recursive: true }, (err, files) => {
+            if (err) throw err;
+            fs.readdir(src, { withFileTypes: true }, (err, files) => {
+                if (err) throw err;
+                files.forEach((file) => {
+                    if (file.isFile()) {
+                        fs.copyFile(path.join(src, file.name), path.join(dest, file.name), (err, files) => {
+                            if (err) throw err;
+                        })
+                    }
                 })
-            }
-        })
-    });
+            });
+        });
+    }
+
 });
 
